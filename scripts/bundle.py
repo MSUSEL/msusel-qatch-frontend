0124576782e8a1d0862cbf0f1979c7731dd1ad31
David Rice <davidrice363@gmail.com>
2019-04-28 23:00:48 -0600

    file cleanup

--- scripts/bundle.py
+++
@@ -1,6 +0,0 @@
-import os
-
-bundleCmd01 = 'browserify public/js/dashboard.js -o public/js/bundle.js'
-bundleCmd01 = 'watchify   public/js/dashboard.js -o public/js/bundle.js'
-
-os.system(bundleCmd01)
